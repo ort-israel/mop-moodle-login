408afd06a6f8ef429a18fa3c54171cbbab4227df
root <root@Mapa-LinuxTest.ortweb.org>
2019-11-11 14:34:49 +0200

    changes to reflect the change of the anonymous user generation om campus

--- mop-moodle-login.php
+++ mop-moodle-login.php
@@ -8,40 +8,60 @@ License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: mop-moodle-login
 */
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined('ABSPATH') or die('No script kiddies please!');
 
-function mop_moodle_login_style() {
-    wp_register_style( 'mop-moodle-login-css', plugins_url( 'style.css', __FILE__ ) );
-    wp_enqueue_style( 'mop-moodle-login-css' );
-}
-add_action( 'wp_enqueue_scripts', 'mop_moodle_login_style' );
+class MopMoodleLogin {
 
-function mop_moodle_login_load_textdomain() {
-    load_plugin_textdomain( 'mop-moodle-login', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' );
-}
-add_action( 'plugins_loaded', 'mop_moodle_login_load_textdomain' );
+    public function init() {
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_style'));
+        add_action('plugins_loaded', array($this, 'load_textdomain'));
+        add_shortcode('moodle_login', array($this, 'moodle_login_shortcode'));
+    }
 
-function mop_add_moodle_login($atts){
-    $a = shortcode_atts( array(
-        'id' => '518',
-    ), $atts );
-    return '
+    function enqueue_style() {
+        wp_register_style('mop-moodle-login-css', plugins_url('style.css', __FILE__));
+        wp_enqueue_style('mop-moodle-login-css');
+    }
+
+    function load_textdomain() {
+        load_plugin_textdomain('mop-moodle-login', false, dirname(plugin_basename(__FILE__)) . '/lang');
+    }
+
+
+    function moodle_login_shortcode($atts) {
+        $a = shortcode_atts(array(
+            'id' => '518',
+        ), $atts);
+        return '
     <div id="login_panel"  dir="rtl">
-      <p tabindex="0" > תוכלו לבחון ידיעותיכם בנושאי התכנית שלמדתם. הכנסו למודל</p>
-            <form id="login_form" action="http://schoolnet.moodle.ort.org.il/ort/courselogin.php?courseid='.$a['id'].'" method="post" >
+      <p tabindex="0" >' . __('Test yourself on what you\'ve learned. Log into Moodle', 'mop-moodle-login') . ' </p>
+            <form id="login_form" action="https://campusdev.ort.org.il/ort/anonymous_user_login.php?courseid=' . $a['id'] . '" method="post" >
                 <div class="username_box row">
-                    <label for="username" id="username_lbl">'.__( 'username', 'mop-moodle-login' ).'</label>
+                    <label for="username" id="username_lbl">' . __('username', 'mop-moodle-login') . '</label>
                     <input type="text" name="username" id="username" size="40">
                 </div>
                 <div class="pass_box row">
-                    <label for="password" id="password_lbl">'.__( 'password', 'mop-moodle-login' ).'</label>
+                    <label for="password" id="password_lbl">' . __('password', 'mop-moodle-login') . '</label>
                     <input type="password" name="password" id="password" size="40">
                 </div>
                 <div class="submit_btn row">
-                    <input id="submit_btn" type="submit" value="'.__( 'login', 'mop-moodle-login' ).'" >
+                    <input id="submit_btn" type="submit" value="' . __('login', 'mop-moodle-login') . '" >
                 </div>
             </form>
 
+        </div>
+        <div id="guest_panel" class="guest_panel">
+            <h2 class="guest_panel_title">' . __('guest', 'mop-moodle-login') . ' </h2>
+            <span id="guestNotification">' . __('The system doesn\'t save guest users\'s grades', 'mop-moodle-login') . '</span>
+            <div class="submit_btn">
+                <a id="guestButton" class="guest_button"
+                   href="http://campusdev.ort.org.il/blocks/anonymous_user/generateuser.php?courseid=' . $a['id'] . '">' . __('connect as guest', 'mop-moodle-login') . '</a>
+            </div>
+        </div>
     </div>
     ';
-} add_shortcode('moodle_login','mop_add_moodle_login');
+    }
+}
+
+$mopMoodleLogin = new MopMoodleLogin();
+$mopMoodleLogin->init();
